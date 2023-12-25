87487e14272a6805189f9aae9b5a71359796e764
Juferis <66511872+Juferis@users.noreply.github.com>
2023-12-25 14:43:59 +0900

    react hook form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,36 +1,19 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
-// function ToDoList() {
-//   const [toDo, setValue] = useState('');
-//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-//     const {
-//       currentTarget: { value },
-//     } = event;
-//     setValue(value);
-//   };
-//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault();
-//     console.log('value!', toDo);
-//   };
-//   return (
-//     <div>
-//       <form onSubmit={onSubmit}>
-//         <input onChange={onChange} value={toDo} placeholder="Write a to do" />
-//         <button>Add</button>
-//       </form>
-//     </div>
-//   );
-// }
-
 function ToDoList() {
-  // watch: form의 값들을 주시한다.
-  const { register, watch } = useForm();
-  console.log('watch', watch());
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const handleValid = (data: IForm) => {
+    console.log('data', data.toDo);
+    setValue('toDo', '');
+  };
   return (
     <div>
-      <form>
-        <input {...register('toDo')} placeholder="Write a to do" />
+      <form onSubmit={handleSubmit(handleValid)}>
+        <input
+          {...register('toDo', { required: 'Please write a To Do' })}
+          placeholder="Write a to do"
+        />
         <button>Add</button>
       </form>
     </div>
@@ -38,3 +21,7 @@ function ToDoList() {
 }
 
 export default ToDoList;
+
+interface IForm {
+  toDo: string;
+}
