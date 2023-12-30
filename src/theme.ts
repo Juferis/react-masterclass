c2489ea4f90d4e3699c318418bf1d5b33c132c23
Juferis <66511872+Juferis@users.noreply.github.com>
2023-12-30 15:44:36 +0900

    init

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,14 @@
 import { DefaultTheme } from 'styled-components';
 
-export const darkTheme: DefaultTheme = {
-    bgColor: "#2f3640",
-    textColor: "#191919",
-    // textColor: "#f5f6fa",
-    accentColor: "#EC9118",
-    cardBgColor: "white",
+export const theme: DefaultTheme = {
+    red: "#E51013",
+    black: {
+        veryDark: "#141414",
+        darker: "#181818",
+        lighter: "#2F2F2F"
+    },
+    white: {
+        lighter: "#fff",
+        darker: "#e5e5e5"
+    }
 };
